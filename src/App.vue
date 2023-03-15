34208a006b55978f2c5827e3a92b2ed68ebb40a3
Fernando Cardoso <101157811+fernandoreki@users.noreply.github.com>
2023-03-15 14:20:19 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Hola mundo"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: rgb(234, 189, 248);
+}
+
+#app {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1b4d7e;
+  margin-top: 60px;
+  background-color: bisque;
+}
+</style>
