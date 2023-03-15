9d6126f1e9eca501c7411354ffb86ba339b618b2
Fernando Cardoso Magaña <fernando.cardoso@scio.local>
2023-03-15 17:47:11 -0600

    hacer una ultima revision

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,62 @@
 <template>
-  <div v-if = "isHelloWorldId">
-    <img alt="Vue logo"  src="./assets/logo.png">
-    <HelloWorld v-if = "isHelloWorldId" msg="Hola mundo"/>
+  <div class="theme">
+    <button class="theme-btn" id="btn-theme" v-on:click="darkMode">Dark mode</button>
   </div>
-  <HelloWorld v-else msg= "Adios mundo"/>
-
-  Lista de estudiantes
-  <ul>
-    <li v-for="(alumno, idx) in alumnos" :key="idx">
-      {{ alumno }}
-    </li>
-  </ul>
+  <div v-if="isHelloWorldId">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld v-if="isHelloWorldId" msg="Hello world" />
+  </div>
+  <HelloWorld v-else msg="Bye world" />
+  <MyHobbies msg="These are my hobbies 😎" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import MyHobbies from './components/MyHobbies.vue';
+
+/* tarea */
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    MyHobbies,
   },
   data() {
     return {
       message: "cómo estás mundo?",
       isHelloWorldId: true,
-      alumnos: ['Ignacio', 'Diego', 'Raul']
+      alumnos: ['Ignacio', 'Diego', 'Raul'],
+      isDarkMode: false,
+      count: 0,
+    }
+  },
+  methods: {
+    letterCounter(name) {
+      return name.length;
+    },
+    darkMode() {
+      this.count ++;
+      if (this.count % 2) {
+        document.body.style.backgroundColor = '#31333D';
+        document.body.style.color = '#F0F2FA';
+        document.getElementById('btn-theme').innerHTML = 'Light mode';
+      } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = '#25629e';
+        document.getElementById('btn-theme').innerHTML = 'Dark mode';
+      }
     }
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap');
+
 body {
-  background-color: rgb(234, 189, 248);
+  font-family: 'Kanit', sans-serif;
+  color: #25629e;
 }
 
 #app {
@@ -41,8 +64,18 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #1b4d7e;
   margin-top: 60px;
-  background-color: bisque;
+}
+
+.theme {
+  text-align: right;
+  padding-right: 5%;
+  padding-top: 2%;
+}
+
+#btn-theme {
+    padding: 5px;
+    border-radius: 5px;
+    font-family: inherit;
 }
 </style>
---
+++ src/components/MyHobbies.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="hobbies">
+        <h1>{{ msg }}</h1>
+        <ul>
+            <li v-for="(hobbie, idx) in hobbies" :key="idx" class="hobbies">
+                {{ hobbie }} <br><br> <button v-on:click="getURL(idx)" class="btn-view">View more</button>
+            </li>
+        </ul>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'MyHobbies',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            hobbies: ['Skateboarding', 'Anime', 'Genshin impact', 'Listening to music'],
+            hobbiesUrl: ['https://www.youtube.com/watch?v=F1sjuey25ug', 'https://www.crunchyroll.com/es/watch/G9DUEPGJ5/asteroid-blues', 'https://genshin.hoyoverse.com/es/home', 'https://www.youtube.com/watch?v=1TW3fbGM1CU'],
+            isDarkMode: false,
+        }
+    },
+    methods: {
+        getURL(idx) {
+            window.open(this.hobbiesUrl[idx], '_blank');
+        }
+    }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap');
+
+body {
+    padding: 10px;
+    font-family: 'Kanit', sans-serif;
+}
+
+h1 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 2px 10px;
+    padding: 15px;
+}
+
+.btn-view {
+    padding: 5px;
+    border-radius: 5px;
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+    font-family: inherit;
+}
+
+.btn-view:hover {
+    padding: 5px;
+    border-radius: 5px;
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
+.hobbies {
+    font-weight: bold;
+}
+
+</style>
